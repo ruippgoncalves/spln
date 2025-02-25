a21522455acd472dbad4ca3060b4f25a6b8ee62d
ruippgoncalves <rgoncalves0320@gmail.com>
2025-02-25 14:28:41 +0000

    JSON

--- TPC4/ftk/ftk/__main__.py
+++ TPC4/ftk/ftk/__main__.py
@@ -1,27 +1,35 @@
 import jjcli
+import json
 from base import Pipeline, Convert2ProbabilityStage
 from probability import RelativeProbabilityPerMillion
 
 def pretty_print(freqs, opts):
     freqs = dict(sorted(freqs.items(), key=lambda item: item[1].value, reverse=True))
+    total = freqs[list(freqs.keys())[0]].total
 
     if '-a' not in opts:
         freqs = {key: RelativeProbabilityPerMillion(val) for key, val in freqs.items()}
 
     if '-m' in opts:
         m = int(opts['-m'])
-        freqs = dict((list(freqs.items())[:m]))
+        freqs = dict(list(freqs.items())[:m])
 
-    for key, val in freqs.items():
-        print(f'{key}\t{val}')
+    freqs = {key: val.value for key, val in freqs.items()}
+    if '-j' in opts:
+        print(json.dumps({'total': total, 'words': freqs}, indent=4))
+    else:
+        print(total)
+        for key, val in freqs.items():
+            print(f'{key}\t{val}')
 
 def main():
     """Options:
         -a: aboslute frequency
         -m N: top N words
+        -j: JSON output
     """
 
-    cl = jjcli.clfilter("am:", doc=main.__doc__)
+    cl = jjcli.clfilter("am:j", doc=main.__doc__)
     pipe = Pipeline()
     pipe.set_reduction(Convert2ProbabilityStage())
 
